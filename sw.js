65538cb2431f5a92ca709da4fe3fa003c7c8b8b8
Clemex73 <146777056+Clemex73@users.noreply.github.com>
2026-01-28 14:28:41 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-/* sw.js - cache simple PWA */
-const CACHE_NAME = "clemex-flechettes-v1";
+const CACHE_NAME = "clemex-flechettes-v6";
 const ASSETS = [
   "./",
   "./index.html",
@@ -18,20 +17,15 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE_NAME ? null : caches.delete(k))))
     )
   );
   self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  const req = event.request;
   event.respondWith(
-    caches.match(req).then((cached) => cached || fetch(req).then((res) => {
-      const copy = res.clone();
-      caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
-      return res;
-    }).catch(() => caches.match("./")))
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
 });
